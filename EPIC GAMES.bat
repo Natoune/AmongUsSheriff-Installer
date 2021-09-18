c6959492e22a3723651d8ea802054d0485900d51
Natoune <samdavaure@icloud.com>
2021-09-18 13:47:37 +0200

    Installer v1

--- EPIC GAMES.bat
+++ EPIC GAMES.bat
@@ -2,20 +2,20 @@
 
 setlocal
 
-@echo ---------------------------------------------------------------
-@echo Telechargement de Among.Us.Sheriff.Mod.v1.24.v2021.6.30s.zip...
-@echo ---------------------------------------------------------------
+@echo ----------------------------------------------------------
+@echo Downloading Among.Us.Sheriff.Mod.v1.24.v2021.6.30s.zip...
+@echo ----------------------------------------------------------
 c:\windows\system32\cscript.exe extract.vbs
-@echo ----------------------------
-@echo Copie des fichiers du jeu...
-@echo ----------------------------
+@echo ----------------------
+@echo Copying game files...
+@echo ----------------------
 xcopy "C:\Program Files\Epic Games\AmongUs" "%cd%\Game" /e /i
 
 cd /d %~dp0
 
-@echo ----------------------------
-@echo Extraction de extract.zip...
-@echo ----------------------------
+@echo ------------------
+@echo Extracting Mod...
+@echo ------------------
 Call :UnZipFile "%cd%\Game" "%cd%\extract.zip"
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%cd%\Among US Sheriff.lnk');$s.TargetPath='%cd%\Game\Among Us.exe';$s.Save()"
 exit /b
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # AmongUsSheriff-Installer
-An installer for the Among Us Sheriff Mod
+An installer for the [Among Us Sheriff Mod](https://github.com/Woodi-dev/Among-Us-Sheriff-Mod) by [@Woodi-dev](https://github.com/Woodi-dev)
+
+## How it works
+
+Donwload the zip file [here]() and extract it.
+Run the STEAM.bat or EPIC GAMES.bat **WITHOUT** administrator permissions.
+
+Wait...
+
+And you can launch the Among Us Sheriff file !
